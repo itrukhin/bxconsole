b64225ade137260fc5e6f89a6331264f7d0bd96b
Ioannes <5526173+itrukhin@users.noreply.github.com>
2023-05-12 14:31:41 +0300

    load Bitrix in safe mode

--- src/Bitrix/Loader.php
+++ src/Bitrix/Loader.php
@@ -25,8 +25,9 @@ class Loader {
             /** @noinspection PhpUnusedLocalVariableInspection */
             $DB, $DBType, $DBHost, $DBLogin, $DBPassword, $DBName, $DBDebug, $DBDebugToFile, $APPLICATION, $USER, $DBSQLServerType;
 
-            define('NO_KEEP_STATISTIC', 'Y');
-            define('NO_AGENT_STATISTIC', 'Y');
+            define('SM_SAFE_MODE', true);
+            define('NO_KEEP_STATISTIC', true);
+            define('NO_AGENT_STATISTIC', true);
             define('NO_AGENT_CHECK', true);
             define('NOT_CHECK_PERMISSIONS', true);
             require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
