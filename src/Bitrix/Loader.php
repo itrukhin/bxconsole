d5d27de2b02de0393a6383680874621500bb9ca8
Ioannes <5526173+itrukhin@users.noreply.github.com>
2023-11-13 17:29:29 +0300

    disable Bitrix\Main\Analytics\Counter

--- src/Bitrix/Loader.php
+++ src/Bitrix/Loader.php
@@ -32,6 +32,10 @@ class Loader {
             define('NOT_CHECK_PERMISSIONS', true);
             require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 
+            if(class_exists('\Bitrix\Main\Analytics\Counter')) {
+                \Bitrix\Main\Analytics\Counter::disable();
+            }
+
             if (defined('B_PROLOG_INCLUDED') && B_PROLOG_INCLUDED === true) {
                 return true;
             }
